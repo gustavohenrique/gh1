d181374888a7a21cf3d29b7459c23b994e234738
Gustavo Henrique <gustavo@gustavohenrique.net>
2015-12-13 15:22:36 -0200

    Fixed cors

--- main.go
+++ main.go
@@ -19,8 +19,10 @@ func main() {
     // CORS
     middle := interpose.New()
     middle.UseHandler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-        w.Header().Set("Access-Control-Allow-Origin", "*")
         w.Header().Set("Content-Type", "application/json")
+        w.Header().Set("Access-Control-Allow-Origin", "*")
+        w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+        w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
     }))
     middle.UseHandler(mux)
 
