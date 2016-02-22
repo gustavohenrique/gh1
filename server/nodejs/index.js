45eb06ec56b8cc88cbb093cd4f4c34dfbda407a8
Gustavo Henrique <gustavo@gustavohenrique.net>
2016-02-21 22:33:59 -0300

    Fixes for heroku deploy

--- server/nodejs/index.js
+++ server/nodejs/index.js
@@ -1,5 +1,5 @@
 var app = require('./src/app');
-var port = process.env.GH1_PORT || 5000;
+var port = process.env.PORT || 5000;
 var ip = process.env.GH1_IP || '0.0.0.0';
 
 app.createServer().listen(port, ip, function () {
